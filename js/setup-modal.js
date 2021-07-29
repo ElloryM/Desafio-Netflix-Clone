bd0cd736cffe5822882d9ae0cd543f4a465474d1
ElloryM <ellorymiralha@gmail.com>
2021-07-29 15:32:25 -0300

    todos os elementos incluidos

---
+++ js/setup-modal.js
@@ -0,0 +1,133 @@
+function acao1() {
+    let modal = document.querySelector('#modal')
+
+    modal.style.display = 'block';
+
+}
+
+function fechar1 () {
+    let modal = document.querySelector('#modal')
+
+    modal.style.display = 'none';
+}
+
+function acao2() {
+    let modal = document.querySelector('#modal2')
+
+    modal.style.display = 'block';
+
+}
+
+function fechar2 () {
+    let modal = document.querySelector('#modal2')
+
+    modal.style.display = 'none';
+}
+
+function acao3() {
+    let modal = document.querySelector('#modal3')
+
+    modal.style.display = 'block';
+
+}
+
+function fechar3 () {
+    let modal = document.querySelector('#modal3')
+
+    modal.style.display = 'none';
+}
+
+function acao4() {
+    let modal = document.querySelector('#modal4')
+
+    modal.style.display = 'block';
+
+}
+
+function fechar4 () {
+    let modal = document.querySelector('#modal4')
+
+    modal.style.display = 'none';
+}
+
+function acao5() {
+    let modal = document.querySelector('#modal5')
+
+    modal.style.display = 'block';
+
+}
+
+function fechar5 () {
+    let modal = document.querySelector('#modal5')
+
+    modal.style.display = 'none';
+}
+
+function acao6() {
+    let modal = document.querySelector('#modal6')
+
+    modal.style.display = 'block';
+
+}
+
+function fechar6 () {
+    let modal = document.querySelector('#modal6')
+
+    modal.style.display = 'none';
+}
+
+function acao7() {
+    let modal = document.querySelector('#modal7')
+
+    modal.style.display = 'block';
+
+}
+
+function fechar7 () {
+    let modal = document.querySelector('#modal7')
+
+    modal.style.display = 'none';
+}
+
+function acao8() {
+    let modal = document.querySelector('#modal8')
+
+    modal.style.display = 'block';
+
+}
+
+function fechar8 () {
+    let modal = document.querySelector('#modal8')
+
+    modal.style.display = 'none';
+}
+
+
+function acao9() {
+    let modal = document.querySelector('#modal9')
+
+    modal.style.display = 'block';
+
+}
+
+function fechar9 () {
+    let modal = document.querySelector('#modal9')
+
+    modal.style.display = 'none';
+}
+
+function acao10() {
+    let modal = document.querySelector('#modal10')
+
+    modal.style.display = 'block';
+
+}
+
+function fechar10 () {
+    let modal = document.querySelector('#modal10')
+
+    modal.style.display = 'none';
+}
+
+
+
